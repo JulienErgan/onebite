31d111be0ecc098d1333f60af8d63614182e7b22
Shogo Hida <shogo.hida@gmail.com>
2020-05-28 22:44:26 +0900

    changed chapters

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -16,13 +16,20 @@ class EnrollmentsController < ApplicationController
   end
 
   def create
+    # creation of enrollment
     @course = Course.find(params[:course_id])
     @enrollment = Enrollment.new(enrollment_params)
     @enrollment.course = @course
     @enrollment.completion_status = 0
     @enrollment.user = current_user
-    @date = params[:start_date]
     authorize @enrollment
+    # creation of chapters
+    @date = params[:start_date]
+    @time = params[:time_of_day]
+    # date timeからdatetimeを作れればその変数をchapter modelに渡せる
+    # raise here to check
+    # チャプターにハードコードでstart_time書いてチャプターのビュー作ればカレンダー表示できるかな
+    #　もしくはもんてローゼでデイリーセット各時刻インスタンスをstart_timeでセットしてチャプターに渡す
     if @enrollment.save
       redirect_to user_path(current_user)
     else
--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -2,4 +2,10 @@ class Chapter < ApplicationRecord
   belongs_to :course
   has_many :chapters
   validates :name, presence: true
+
+  def start_time
+    self.course.enrollments.last.start_date.start ##Where 'start' is a attribute of type 'Date' accessible through MyModel's relationship
+  　#　start_date to start_time　
+  end
+
 end
